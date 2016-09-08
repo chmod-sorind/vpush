1a9118c2ffde6d6594f04564d115779a60d843f3
Sorin Diaconescu <diacconescu.sorin.g@gmail.com>
2016-09-08 18:08:49 -0400

    Added more parameters for arguments.

--- vpushRefactor.py
+++ vpushRefactor.py
@@ -8,5 +8,5 @@ parser.add_argument("ip", metavar='IP', type=str, nargs='?', default="",
 parser.add_argument("--bsp", "-p", action="store_true", help="Push a poll on a BroadSign Player on port 2323 by default.")
 parser.add_argument("--bses", "-e", action="store_true", help="Push a poll on a BroadSign Edge Server on port 2324 by default.")
 parser.add_argument("--port", action="store_true", help="Specify a different port for telnet connection.")
-
+parser.add_argument("--port", action="store_true", help="Specify a different port for telnet connection.")
 args = parser.parse_args()
