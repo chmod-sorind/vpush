ed59821edcfcd37b462e18ac1c5c0836ef73eb4d
Sorin Diaconescu <diacconescu.sorin.g@gmail.com>
2016-09-08 18:06:35 -0400

    Added more parameters for arguments.

--- vpushRefactor.py
+++ vpushRefactor.py
@@ -5,7 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description="Force a poll on BroadSign Player/Edge Server")
 parser.add_argument("ip", metavar='IP', type=str, nargs='?', default="",
                     help="Target IP Address.")
-parser.add_argument("--bsp", "-p", action="store_true", help="Push a poll on a BroadSign Player")
-parser.add_argument("--bses", "-e", action="store_true", help="Push a poll on a BroadSign Edge Serger")
+parser.add_argument("--bsp", "-p", action="store_true", help="Push a poll on a BroadSign Player on port 2323 by default.")
+parser.add_argument("--bses", "-e", action="store_true", help="Push a poll on a BroadSign Edge Server on port 2324 by default.")
+parser.add_argument("--port", action="store_true", help="Specify a different port for telnet connection.")
 
 args = parser.parse_args()
