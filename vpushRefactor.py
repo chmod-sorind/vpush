bb950974dbdbb3cdd071142d09fbef824d4b8008
Matei Bianca <Bianca@Mateis-MacBook-Air.local>
2016-09-22 23:25:05 -0400

    Fixed the check for --file option and append the content to TARGET_HOST_IP. Fix the check for --ip and append all items to TARGET_HOST_IP correctly.

--- vpushRefactor.py
+++ vpushRefactor.py
@@ -8,7 +8,6 @@ TARGET_HOST_IP = []
 TARGET_HOST_PORT = ''
 POLLING_COUNT = ''
 POLLING_RATE = ''
-IP_FILE = ''
 VERSION = "0.2.0"
 '''
 V 0.2.0
@@ -23,9 +22,9 @@ parser.add_argument('command', metavar='COMMAND', type=str, nargs='?', default='
 parser.add_argument('--bsp', action='store_true', help='Push a poll on a BroadSign Player on port 2323 by default.')
 parser.add_argument('--bses', action='store_true', help='Push a poll on a BroadSign Edge Server on port 2324 by default.')
 parser.add_argument('--port', '-p', action='store', type=int, help="Specify a different port for the telnet connection (default: bsp[{1}], bses[{0}]).".format(PORT_NUMBER_LIST[0], PORT_NUMBER_LIST[1]))
-parser.add_argument('--ip', '-t', nargs='+', help='Specify one target IP or a list of IPs in a comma separated list.')
+parser.add_argument('--ip', '-t', nargs='+', help='Specify one target IP or a list of IPs.')
 parser.add_argument('--file', '-f', action='store', type=argparse.FileType('r'), help='Specify a file that contains a list of IPs (one IP per row).')  # type=argparse.FileType('r'), default=sys.stdin
-parser.add_argument('--frequency', '-fq', default=1, type=float, help='Specify the frequency of the poll in seconds. Default value is 30.')
+parser.add_argument('--frequency', '-fq', default=30, type=float, help='Specify the frequency of the poll in seconds. Default value is 60.')
 parser.add_argument('--count', '-c', default=1, type=int, help='Specify the number of repetitions. Default value is 1.')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s ' + VERSION)
 args = parser.parse_args()
@@ -34,7 +33,7 @@ if __name__ == '__main__':
 
     def query_yes_no(question, default="yes"):
         valid = {"yes": True, "y": True, "ye": True,
-                 "no": False, "n": False}
+                 "no": False, "n": False, "bsp": False}  # try to change yes/no query
         if default is None:
             prompt = " [y/n] "
         elif default == "yes":
@@ -65,18 +64,12 @@ if __name__ == '__main__':
         quit(print("Script failed..."))
 
     if args.ip is not None:
-        TARGET_HOST_IP.append(args.ip)
+        for ip in args.ip:
+            TARGET_HOST_IP.append(ip)
     elif args.file is not None:
-        FILE_CONTENT = args.file.readline().strip()
-        print(FILE_CONTENT)
-"""
-        #print(args.file)
-        #with open(args.file) as IP:
-            #line = IP.read().splitline()
-            #for f in line:
-                #TARGET_HOST_IP.append(f)
-http://www.programcreek.com/python/example/5080/argparse.FileType # Something to read.
-"""
+        for ip in args.file.readlines():
+            line_ip = ip.rstrip()
+            TARGET_HOST_IP.append(line_ip)
 
     if args.port is None and args.bsp is False and args.bses is False:
         print("Please select at least one option for target port (--port/-p [PORT] OR --bsp OR --bses)")
